2e280731f60bdb68c07d3d3ee4f9f67d3164120f
Li Pu <825369365@qq.com>
2020-02-22 16:29:55 +0800

    feat(222): 完全二叉树的节点个数
    [js] 2nd commit

--- questions/222/js/222.完全二叉树的节点个数.js
+++ questions/222/js/222.完全二叉树的节点个数.js
@@ -1,9 +1,14 @@
 /**
  * @param {TreeNode} root
  * @return {number}
- * time: 124ms space: 45.9MB
+ * time: 104ms space: 45MB
  */
+var getHeight = root => (root === null ? 0 : getHeight(root.left) + 1);
 var countNodes = function(root) {
     if (root === null) return 0;
-    return countNodes(root.left) + countNodes(root.right) + 1;
+    let leftheight = getHeight(root.left);
+    let rightHeight = getHeight(root.right);
+    return leftheight === rightHeight
+        ? (1 << leftheight) + countNodes(root.right)
+        : countNodes(root.left) + (1 << rightHeight);
 };
