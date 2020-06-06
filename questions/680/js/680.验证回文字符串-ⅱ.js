564de334fa846f69262de77ce4f611bf53d47cc7
Li Pu <825369365@qq.com>
2020-06-07 05:54:35 +0800

    feat(680): 验证回文字符串 Ⅱ
    [js] 2nd commit

--- questions/680/js/680.验证回文字符串-ⅱ.js
+++ questions/680/js/680.验证回文字符串-ⅱ.js
@@ -1,19 +1,19 @@
 /**
  * @param {string} s
  * @return {boolean}
- * time: 112ms space: 43.2MB
+ * time: 84ms space: 41.6MB
  */
-var validPalindrome = function(s) {
-    let deleteFlag = false;
+var validPalindrome = function (s) {
     const test = (str, start, end) => {
-        if(start >= end) return true;
-        else if(str[start] === str[end]) return test(str, start+ 1, end-1);
-        else if(!deleteFlag) {
-            deleteFlag = true;
-            return test(str, start + 1, end) || test(str, start, end -1);
-        }else{
-            return false;
+        while (start < end) {
+            if (str[start++] != str[end--]) return false;
+        }
+        return true;
+    };
+    for (let i = 0, j = s.length - 1; i < j; i++, j--) {
+        if (s[i] !== s[j]) {
+            return test(s, i + 1, j) || test(s, i, j - 1);
         }
     }
-    return test(s, 0, s.length -1);
+    return true;
 };
