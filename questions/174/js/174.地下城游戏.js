03bd11563eddd1a84ec927c8c244595780c6f29e
Li Pu <825369365@qq.com>
2020-07-12 16:47:57 +0800

    feat(174): 地下城游戏
    [js] 2nd commit

--- questions/174/js/174.地下城游戏.js
+++ questions/174/js/174.地下城游戏.js
@@ -1,28 +1,27 @@
 /**
  * @param {number[][]} dungeon
  * @return {number}
- * time: 88ms space: 37.3MB
+ * time: 76ms space:36.4MB
  */
 var calculateMinimumHP = function (dungeon) {
     let rlen = dungeon.length;
     let clen = dungeon[0].length;
-    let matrix = Array(rlen).fill(0).map(row => Array(clen).fill(0));
-    for (let i = rlen - 1; i >= 0; i--){
-        for (let j = clen - 1; j >= 0; j--){
-            if (i === rlen - 1) {
-                if (j === clen - 1) {
-                    matrix[i][j] = Math.max(-dungeon[i][j], 0);
-                }
-                else {
-                    matrix[i][j] = Math.max(matrix[i][j + 1] - dungeon[i][j], 0);
-                }
-            } else if (j === clen - 1) {
-                matrix[i][j] =  Math.max(matrix[i + 1][j] - dungeon[i][j], 0);
-            } else {
-                matrix[i][j] = Math.max(Math.min(matrix[i][j + 1] - dungeon[i][j], matrix[i + 1][j] - dungeon[i][j]), 0)
-            }
+    for (let i = rlen - 1; i >= 0; i--) {
+        for (let j = clen - 1; j >= 0; j--) {
+            dungeon[i][j] = Math.max(
+                i === rlen - 1
+                    ? j === clen - 1
+                        ? -dungeon[i][j]
+                        : dungeon[i][j + 1] - dungeon[i][j]
+                    : j === clen - 1
+                    ? dungeon[i + 1][j] - dungeon[i][j]
+                    : Math.min(
+                          dungeon[i][j + 1] - dungeon[i][j],
+                          dungeon[i + 1][j] - dungeon[i][j]
+                      ),
+                0
+            );
         }
     }
-    return matrix[0][0] + 1;
+    return dungeon[0][0] + 1;
 };
-
