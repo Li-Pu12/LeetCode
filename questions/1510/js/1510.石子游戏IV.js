4b5d76f2663cc0275e4f09cc860de2411dc61b3b
Li Pu <825369365@qq.com>
2020-07-21 15:14:35 +0800

    feat(1510): 石子游戏IV
    [js] 2nd commit

--- questions/1510/js/1510.石子游戏IV.js
+++ questions/1510/js/1510.石子游戏IV.js
@@ -1,13 +1,13 @@
 /**
  * @param {number} n
  * @return {boolean}
- * time: 140ms space: 40MB
+ * time: 132ms space: 40.1MB
  */
 var winnerSquareGame = function (n) {
     let res = Array(n + 1).fill(false);
     for (let i = 0; i <= n; i++) {
-        for (let j = 1; j * j <= i; j++) {
-            if (!res[i - j * j]) {
+        for (let j = 1, j2 = 1; j2 <= i; j++, j2 = j * j) {
+            if (!res[i - j2]) {
                 res[i] = true;
                 break;
             }
