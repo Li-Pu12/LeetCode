871c445dbb82975f7db52f2d9eee49dd2ba8a55c
Li Pu <825369365@qq.com>
2020-07-24 17:31:48 +0800

    feat(1025): 除数博弈
    [js] 2nd commit

--- questions/1025/js/1025.除数博弈.js
+++ questions/1025/js/1025.除数博弈.js
@@ -1,22 +1,6 @@
 /**
  * @param {number} N
  * @return {boolean}
- * time: 88ms space: 38.5MB
+ * time: 80ms space: 37.6MB
  */
-var divisorGame = function (N) {
-    let res = Array(N + 1)
-        .fill(false)
-        .map(() => [1]);
-    res[0] = res[1] = false;
-    for (let i = 2; i <= N; i++) {
-        let t = false;
-        for (let j = 0; j < res[i].length; j++) {
-            if (!res[i - res[i][j]]) {
-                t = true;
-                break;
-            }
-        }
-        res[i] = t;
-    }
-    return res[N];
-};
+var divisorGame = N => !(N & 1);
