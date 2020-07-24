075c4865f4439034eee68b2bb9088392248fdaec
Li Pu <825369365@qq.com>
2020-07-24 19:22:06 +0800

    feat(912): 排序数组
    [js] 3rd commit 冒泡排序2

--- questions/912/js/912.排序数组.js
+++ questions/912/js/912.排序数组.js
@@ -1,18 +1,23 @@
 /**
  * @param {number[]} nums
  * @return {number[]}
- * time: 4488ms space: 42.4MB
+ * time: 4432ms space: 42.5MB
  */
 var sortArray = function (nums) {
     // 冒泡排序
+    let flag;
+    let t;
     for (let i = 0; i < nums.length; i++) {
+        flag = false;
         for (let j = nums.length - 1; j > i; j--) {
             if (nums[j] < nums[j - 1]) {
-                let t = nums[j - 1];
+                t = nums[j - 1];
                 nums[j - 1] = nums[j];
                 nums[j] = t;
+                flag = true;
             }
         }
+        if (!flag) break;
     }
     return nums;
 };
