428c2d61d997461eead47b88e5038c3cc862582e
Li Pu <825369365@qq.com>
2020-07-30 13:47:20 +0800

    feat(343): 整数拆分
    [js] 2nd commit

--- questions/343/js/343.整数拆分.js
+++ questions/343/js/343.整数拆分.js
@@ -1,15 +1,18 @@
 /**
  * @param {number} n
  * @return {number}
- * time: 92ms space: 37.5MB
+ * time: 80ms space: 37.6MB
  */
 var integerBreak = function (n) {
     let res = Array(n + 1).fill(0);
     res[1] = 1;
     for (let i = 2; i <= n; i++) {
+        let t = 0;
         for (let j = 1; j <= Math.floor(i / 2); j++) {
-            res[i] = Math.max(res[i], j * Math.max(i - j, res[i - j]));
+            let k = j * res[i - j];
+            if (k > t) t = k;
         }
+        res[i] = i !== n && i > t ? i : t;
     }
     return res[n];
 };
